4e6223a2f6c16ee5dfcdba24bc1487170fe4f152
NessaDevelops <nessa.develops@gmail.com>
2020-01-29 01:09:46 -0700

    8. Manage Local State

--- start/client/src/index.jsx
+++ start/client/src/index.jsx
@@ -1,11 +1,14 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 import React from "react";
 import ReactDOM from "react-dom";
+import Login from "./pages/login";
 import Pages from "./pages";
 import injectStyles from "./styles";
+import { resolvers, typeDefs } from "./resolvers";
+import gql from "graphql-tag";
 
 const cache = new InMemoryCache();
 const client = new ApolloClient({
@@ -14,6 +17,8 @@ const client = new ApolloClient({
     headers: { authorization: localStorage.getItem('token') },
     uri: "http://localhost:4000/graphql",
   }),
+  typeDefs,
+  resolvers
 });
 
 cache.writeData({
@@ -23,10 +28,21 @@ cache.writeData({
   },
 });
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
 injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById("root")
 );
